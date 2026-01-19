9024b44491673e9c2d3dd5aab2cc902a8305a77e
S3B4S5 <sebastian75601012@outlook.com>
2026-01-18 22:47:21 -0400

    asd

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
+  app.enableCors({
+    origin: ['https://primer-parcial-sw-1-clases.web.app/'], // Angular dev server
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix('api');
-  
   await app.listen(process.env.PORT ? Number(process.env.PORT) : 3000);
 }
 bootstrap();
